e7f2d261a10fdb08f97ea09692809d623a78143e
jeetu-mj <jeetu.mj123@gmail.com>
2025-04-26 15:24:33 +0530

    user model update

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -178,19 +178,6 @@ const userSchema = new mongoose.Schema({
 }, {
   timestamps: true
 });
-
-// Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
-});
-
-// Method to check password
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
-};
-
 const User = mongoose.model('User', userSchema);
 
 export default User;
