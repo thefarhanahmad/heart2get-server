ea6c1083172af6bd900550cbddb390eb54a675d9
farhan <akhtarfarhan281@gmail.com>
2025-06-18 21:11:55 +0530

    paypal

--- src/utils/paypal.js
+++ src/utils/paypal.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 // Load environment variables from .env file
 dotenv.config();
 
-const environment = new paypal.core.SandboxEnvironment(
+const environment = new paypal.core.LiveEnvironment(
   process.env.PAYPAL_CLIENT_ID,
   process.env.PAYPAL_SECRET
 );
